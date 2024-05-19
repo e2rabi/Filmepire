25ac79ceaf0ef96bc7053c283a481d55dbbe6972
errabi ayoub <errabi.ayoub@gmail.com>
2024-05-19 18:02:33 +0100

    Add base components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material'
 import { Switch, Route } from 'react-router-dom'
-
+import { Actors, Movies, MovieInformation, Profile, NavBar } from './'
 function App() {
   return (
     <div>
       <CssBaseline />
       <main>
+        <NavBar />
         <Switch>
           <Route exact path="/">
-            <h1>Home</h1>
+            <Movies />
           </Route>
           <Route excat path="/movies">
-            <h1>Movies</h1>
+            <Movies />
           </Route>
           <Route excat path="/movie/:id">
-            <h1>Movies informations</h1>
+            <MovieInformation />
           </Route>
           <Route excat path="/actor/:id">
-            <h1>Actors informations</h1>
+            <Actors />
           </Route>
           <Route excat path="/profile/:id">
-            <h1>Profile informations</h1>
+            <Profile />
           </Route>
         </Switch>
       </main>
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieInformation = () => {
+    return (
+        <div>
+            Movie informations
+        </div>
+    )
+}
+
+export default MovieInformation
