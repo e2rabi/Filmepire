99b155f831c101722969fc9433f4da802154f513
errabi ayoub <errabi.ayoub@gmail.com>
2024-05-23 00:00:38 +0100

    create navBar component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
-
+const theme = createTheme({});
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
